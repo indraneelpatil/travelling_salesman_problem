0ca331b5ab782ea9618d94d249f2db2e8a8068a5
Indraneel Patil <Neil@Neils-MacBook-Pro.local>
2023-05-29 09:33:37 -0400

    Vertices are now floats which ensures unique edge weights and added max weight parameter on edges

--- graph_lib/src/lib.rs
+++ graph_lib/src/lib.rs
@@ -6,13 +6,13 @@ use rand::Rng;
 pub struct Edge {
     endpoint1 : u32,
     endpoint2 : u32,
-    weight : u32
+    weight : f64
 }
 
 #[derive(Debug)]
 pub struct Vertex {
-    pub x : i32,
-    pub y : i32
+    pub x : f64,
+    pub y : f64
 }
 
 #[derive(Debug)]
@@ -38,7 +38,7 @@ impl Graph {
         chart.draw_series(
             self.vertices
                 .iter()
-                .map(|vertex| Circle::new((vertex.x, vertex.y), 5, ShapeStyle::from(&BLACK).filled())),
+                .map(|vertex| Circle::new((vertex.x as i32, vertex.y as i32), 5, ShapeStyle::from(&BLACK).filled())),
         )?;
 
         // Plot edges
@@ -47,7 +47,7 @@ impl Graph {
             let endpoint2 = &self.vertices[edge.endpoint2 as usize];
 
             chart.draw_series(LineSeries::new(
-                vec![(endpoint1.x, endpoint1.y), (endpoint2.x, endpoint2.y)],
+                vec![(endpoint1.x as i32, endpoint1.y as i32), (endpoint2.x as i32, endpoint2.y as i32)],
                 &RED,
             ))?;
         }
@@ -61,9 +61,9 @@ impl Graph {
         loop {
 
             // Generate x index
-            let rand_x = rand::thread_rng().gen_range(1..=100);
+            let rand_x = rand::thread_rng().gen_range(1.0..=100.0);
             // Generate y index
-            let rand_y = rand::thread_rng().gen_range(1..=100);
+            let rand_y = rand::thread_rng().gen_range(1.0..=100.0);
 
             let rand_vert = Vertex {
                 x : rand_x,
@@ -80,7 +80,14 @@ impl Graph {
         }
     }
 
-    fn generate_edges(&mut self, num_edges: u32, num_vertices: u32, max_weight: u32) {
+    fn euclidean_distance(&self,x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
+        let x_diff = x2 - x1;
+        let y_diff = y2 - y1;
+        let distance_squared = x_diff * x_diff + y_diff * y_diff;
+        distance_squared.sqrt()
+    }
+
+    fn generate_edges(&mut self, num_edges: u32, num_vertices: u32, max_weight: f64) {
         
         let mut generated_weights = Vec::new();
         
@@ -90,7 +97,8 @@ impl Graph {
             // weight is manhattan distance between the two vertices
             let pt1 = &self.vertices[endpoint1 as usize];
             let pt2 = &self.vertices[endpoint2 as usize];
-            let weight = rand::thread_rng().gen_range(0..=max_weight);
+            let weight = self.euclidean_distance(pt1.x, pt1.y, pt2.x, pt2.y);
+            //let weight = rand::thread_rng().gen_range(0..=max_weight);
             
             let edge = Edge {
                 endpoint1,
@@ -98,7 +106,7 @@ impl Graph {
                 weight,
             };
             
-            if !self.is_duplicate_edge(&edge) && !generated_weights.contains(&weight) {
+            if !self.is_duplicate_edge(&edge) && !generated_weights.contains(&weight) && edge.weight < max_weight{
                 self.edges.push(edge);
                 generated_weights.push(weight);
             }
@@ -116,14 +124,15 @@ impl Graph {
     pub fn randomise_graph(&mut self) {
 
         // parameters
-        let tot_edges  = 50;
+        let tot_edges  = 100;
         let tot_vertices = 50; 
+        let max_weight = 30.0;
 
         // Add random vertices to the graph
         self.generate_vertices(tot_vertices);
 
         // Add random edges to the graph 
-        self.generate_edges(tot_edges,tot_vertices,1000);
+        self.generate_edges(tot_edges,tot_vertices,max_weight);
 
 
     }
